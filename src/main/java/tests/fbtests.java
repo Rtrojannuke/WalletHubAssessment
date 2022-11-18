c2cbcf681b25009037c91e0aa72102d6d594db96
nuke <mubarak.arimiyah@sterling.ng>
2022-11-18 12:29:54 +0100

    added XML Runner

--- src/main/java/tests/fbtests.java
+++ src/main/java/tests/fbtests.java
@@ -9,13 +9,20 @@ import pages.fb_login;
 
 public class fbtests extends TestBase{
 	utility utils = new utility();
+	fb_login login = new fb_login(driver);
 	
 	@Test(dataProvider = "facebook")
 	public void loginTest(String username, String password) throws Exception {
-		fb_login login = new fb_login(driver);
 		login.inputEmail(username);
 		login.inputPassword(password);
 		login.clickLogin();
+		login.clickCreatePost();
+		login.createPostContent();
+		login.postToUser();
+	}
+	
+	@Test
+	public void createPost(String username, String password) throws Exception {
 		login.clickCreatePost();
 		login.createPostContent();
 		login.postToUser();
